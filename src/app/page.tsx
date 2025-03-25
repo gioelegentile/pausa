10e19b2c7fccb4b90b87c9588778b457fa143914
Ivan Antonino Pace <ivanantoninopace@gmail.com>
2025-03-25 17:19:44 +0100

    removed logo from layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 import { Search } from "~/app/_components/search";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
-
-
+import Logo from "./_components/logo";
 
 export default async function Home() {
   const session = await auth();
@@ -12,19 +11,19 @@ export default async function Home() {
   if (session?.user) {
     void api.work.getAllRated.prefetch([
       {
-        orderBy: 'createdAt',
-        orderDirection: 'desc'
-      }
+        orderBy: "createdAt",
+        orderDirection: "desc",
+      },
     ]);
   }
 
   return (
-<HydrateClient>
-  <main className="relative h-screen">
-
-  <div className="absolute top-0 left-0 w-full h-full z-0">
+    <HydrateClient>
+      <main className="relative h-screen">
+      <Logo />
+        <div className="absolute top-0 left-0 z-0 h-full w-full">
           <video
-            className="w-full h-full object-cover"
+            className="h-full w-full object-cover"
             src="/background_video.mp4"
             autoPlay
             loop
@@ -33,24 +32,22 @@ export default async function Home() {
           />
         </div>
 
+        <div className="relative">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <p className="text-center text-2xl text-white">
+              {session && <span>Logged in as {session.user?.name}</span>}
+            </p>
+            <Link
+              href={session ? "/api/auth/signout" : "/api/auth/signin"}
+              className="absolute top-4 right-4 me-2 mb-2 rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+            >
+              {session ? "Sign out" : "Sign in"}
+            </Link>
+          </div>
 
-
-    <div className="relative">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p className="text-center text-2xl text-white">
-          {session && <span>Logged in as {session.user?.name}</span>}
-        </p>
-        <Link
-          href={session ? "/api/auth/signout" : "/api/auth/signin"}
-          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 absolute top-4 right-4"
-        >
-          {session ? "Sign out" : "Sign in"}
-        </Link>
-      </div>
-
-      {session?.user && <Search />}
-    </div>
-  </main>
-</HydrateClient>
+          {session?.user && <Search />}
+        </div>
+      </main>
+    </HydrateClient>
   );
 }
