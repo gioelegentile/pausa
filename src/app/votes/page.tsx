5c0ad4785318e06dd5482358ae48cb8c996164ef
Gioele Gentile <gioele.gentile@webscience.it>
2025-04-09 12:21:43 +0200

    refactor: remove unused imports and commented code in page.tsx

--- src/app/votes/page.tsx
+++ src/app/votes/page.tsx
@@ -1,18 +1,7 @@
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import { VotesContent } from "~/app/_components/votes-content";
 
-export const dynamic = "force-dynamic";
-
-async function waitFor(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default async function Votes() {
-// await waitFor(4000)
-    const a = await api.work.getAllRatedByType({ 
-      type: "movie",
-      sorting: [],
-    });
   return (
     <HydrateClient>
       <VotesContent />
