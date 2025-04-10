0950268ce97dfee11beee19213a40d573fcd41fd
Gioele Gentile <gioele.gentile@webscience.it>
2025-04-10 11:33:56 +0200

    feat: implement search functionality with media type selection and debounce hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { HydrateClient } from "~/trpc/server";
-import { Search } from "./_components/search";
+import {redirect} from "next/navigation";
 
-export default async function Home() {
-  return (
-    <HydrateClient>
-      <div className="flex-1">
-        <div className="relative">
-          <div className="flex flex-col items-center justify-center gap-4">
-            <Search />
-          </div>
-        </div>
-      </div>
-    </HydrateClient>
-  );
+export default function Home() {
+    redirect('/search/movie');
 }
