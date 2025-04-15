f2cf06ad51d34cb9d820ad0c237a2da3a636b5a8
Gioele Gentile <gioelegentile.it@gmail.com>
2025-04-15 20:19:08 +0200

    feat: wrap children in a div for improved layout consistency in VotesPageLayout

--- src/app/votes/[mediaType]/layout.tsx
+++ src/app/votes/[mediaType]/layout.tsx
@@ -76,7 +76,10 @@ export default async function VotesPageLayout({
             </div>
           </div>
 
-          {children}
+          <div className="mx-auto w-full max-w-7xl px-4  sm:px-6 lg:px-8">
+            {children}
+          </div>
+          
         </div>
       </div>
     </div>
